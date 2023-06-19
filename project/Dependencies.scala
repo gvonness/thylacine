9f09c69b0c9ef5ff4bfde7aed0ef3c7353859c9b
Greg von Nessi <greg.vonnessi@entrolution.ai>
2023-06-19 12:12:06 +0100

    STM update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object DependencyVersions {
   val scala2p13Version = "2.13.8"
 
-  val bengalStmVersion           = "0.7.1-SNAPSHOT-22-b9c415e"
+  val bengalStmVersion           = "0.7.1-SNAPSHOT-23-2104f3e"
   val bigMathVersion             = "2.3.0"
   val breezeVersion              = "2.0"
   val catsEffectVersion          = "3.3.14"
