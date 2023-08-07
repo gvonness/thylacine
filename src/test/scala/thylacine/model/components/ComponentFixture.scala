90eace7f097f5979cbfe2f5f30504eb6cdcc9906
Greg von Nessi <greg.vonnessi@entrolution.ai>
2023-08-07 15:58:01 +0100

    Stateless optimisers

--- src/test/scala/thylacine/model/components/ComponentFixture.scala
+++ src/test/scala/thylacine/model/components/ComponentFixture.scala
@@ -179,7 +179,7 @@ object ComponentFixture {
     convergenceThreshold      = 1e-20,
     goldenSectionTolerance    = 1e-10,
     lineProbeExpansionFactor  = 2.0,
-    minimumNumberOfIterations = 1400
+    minimumNumberOfIterations = 2000
   )
 
   def conjugateGradientOptimisedPosteriorF(implicit stm: STM[IO]): IO[ConjugateGradientOptimisedPosterior[IO]] =
